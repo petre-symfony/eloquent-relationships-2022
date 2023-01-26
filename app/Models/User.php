40a1c1d7ffb5c4a78362b76f71bae2103dd1b030
petrero <petrero@gmail.com>
2023-01-26 15:23:25 +0200

    1.1 OneToOne relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable {
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile(){
+        return $this->hasOne(Profile::class);
+    }
+
+    public function experience(){
+        return $this->hasOne(Experience::class);
+    }
 }
