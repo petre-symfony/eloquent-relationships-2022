267d95ee5f452eecdc81437767ee852b128113b5
petrero <petrero@gmail.com>
2023-01-30 22:00:54 +0200

    3.3 many to many relation Register the timestamp in pivot table. Create a primary key to not associate a tag and a post twice and delete the value in the pivot table too when a tag or a post is deleted

--- database/migrations/2023_01_30_190931_create_post_tag_table.php
+++ database/migrations/2023_01_30_190931_create_post_tag_table.php
@@ -12,9 +12,12 @@ return new class extends Migration {
      */
     public function up() {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
+            $table->primary(['post_id', 'tag_id']);
             $table->unsignedInteger('post_id');
             $table->unsignedInteger('tag_id');
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->timestamps();
         });
     }
