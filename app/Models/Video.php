4bdf062bfb42e903df402ae241f57ad67bcf9af4
petrero <petrero@gmail.com>
2023-02-02 20:20:43 +0200

    5.4 If we want the method name to be unique compared to what the actual polymorphic name was, we got to be explicit in morphTo function

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model {
     use HasFactory;
 
-    public function watchable(){
-        return $this->morphTo();
+    public function parent(){
+        return $this->morphTo('watchable');
     }
 }
